db90e0c9d5759a4874464395a4b16c62ebd625bd
Ivan.Orlov <ivan@morrr.com>
2017-03-10 10:20:16 +0100

    table view

--- dist/example/gulpfile.js
+++ dist/example/gulpfile.js
@@ -1,8 +1,19 @@
 'use strict';
-var gulp_changelog_generator_1 = require('../gulp-changelog-generator');
+Object.defineProperty(exports, "__esModule", { value: true });
+var gulp_changelog_generator_1 = require("../gulp-changelog-generator");
+var path = require("path");
+var fs = require("fs");
+var homedir = require("homedir");
 var gulp = require('gulp');
+var getGitHubToken = function () {
+    var f = path.join(homedir(), ".github-token");
+    if (fs.existsSync(f)) {
+        return fs.readFileSync(f).toString().trim();
+    }
+};
 gulp.task('default', function () {
     var config = {
+        token: getGitHubToken(),
         repoOwner: 'fuse-box',
         repoName: 'fuse-box'
     };
